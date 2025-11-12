334650229b21ebbfd346294f9c5071bbf711d07b
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2025-11-13 02:06:53 +0900

    디자인 패턴 코드 - 팩토리 패턴 숙제 1 완료 - 테스트 로거 추가하여 코드 작동 확인 - 클라이언트 클래스에서 사용자가 입력하여 환경을 설정할 수 있도록 Scanner를 사용

--- DesignPattern/src/FactoryPattern/homework1/FileLogger.java
+++ DesignPattern/src/FactoryPattern/homework1/FileLogger.java
@@ -1,6 +1,7 @@
 package FactoryPattern.homework1;
 
 public class FileLogger implements Logger {
+
     @Override
     public void log(String message){
         System.out.println("파일로그: " + message);
--- DesignPattern/src/FactoryPattern/homework1/LoggerFactory.java
+++ DesignPattern/src/FactoryPattern/homework1/LoggerFactory.java
@@ -6,10 +6,12 @@ public class LoggerFactory {
 
         if(message.equals("file")){
             logger = new FileLogger();
-        } else if(message.equals("console")){
+        } else if(message.equals("env")){  // 운영(console)
             logger = new ConsoleLogger();
-        } else if(message.equals("cloud")){
+        } else if(message.equals("cloud")) {
             logger = new CloudLogger();
+        } else if(message.equals("test")){
+            logger = new TestLogger();
         }
         return logger;
     }
