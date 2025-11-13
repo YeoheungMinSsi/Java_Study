af4b5cf11d77ae6b9ce953a96ced237ac05a772e
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2025-11-14 04:19:07 +0900

    디자인 패턴 코드 ### 빌더 패턴 수정 1. 인터페이스 의존성을 높이기 위해 2개의 인터페이스로 나눔

--- DesignPattern/src/BuilderPattern/homework1/Builder.java
+++ DesignPattern/src/BuilderPattern/homework1/Builder.java
@@ -1,12 +1,5 @@
 package BuilderPattern.homework1;
 
-//
 public interface Builder {
-    Builder select(String... columns);
-    Builder from(String table);
-    Builder where(String condition);
-    Builder orderBy(String column);  // DESC
-    Builder orderBy(String column, String direction);  // ASC
-    Builder limit(int count);
     String build();
 }
--- DesignPattern/src/BuilderPattern/homework1/SelectQueryBuilder.java
+++ DesignPattern/src/BuilderPattern/homework1/SelectQueryBuilder.java
@@ -4,67 +4,72 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SelectQueryBuilder implements Builder {
+public class SelectQueryBuilder implements ISelectBuilder {
 
-    public List<String> selectPart = new ArrayList<>();
-    public String fromPart;
-    public String wherePart;
-    public String orderByPart;
-    public int limitPart;
+    private List<String> _selectParts = new ArrayList<>();
+    private String _fromPart;
+    // note: where절 테스트 AND 병합 -> 단일 타입으로 안됨
+//    public String wherePart;
+    private List<String> _whereParts = new ArrayList<>();
+    private String _orderByPart;
+    private int _limitPart;
 
     @Override
     public SelectQueryBuilder select(String... columns) {
-//        for(String column : columns) {
-//            selectPart.add(column);
-//        }
         if(columns.length > 0)  // 간결성
-//            selectPart.addAll(List.of(columns));  // 불변 리스트 반환
-            selectPart.addAll(Arrays.asList(columns));
+            _selectParts.addAll(Arrays.asList(columns));
         return this;
     }
+
     @Override
     public SelectQueryBuilder from(String table){
-        this.fromPart = table;
+        this._fromPart = table;
         return this;
     }
+
     @Override
-    public SelectQueryBuilder where(String condition){
-        this.wherePart = condition;
+    public SelectQueryBuilder where(String... condition){
+        if (condition.length > 0)  // note: where절 여러번 호출
+            _whereParts.addAll(Arrays.asList(condition));
         return this;
     }
+
     @Override
     public SelectQueryBuilder orderBy(String column){
-        this.orderByPart = column;
+        this._orderByPart = column + " ASC";
         return this;
     }
+
     @Override
     public SelectQueryBuilder orderBy(String column, String direction){
-        this.orderByPart = column + " " + direction;
+        this._orderByPart = column + " " + direction;
         return this;
     }
+
     @Override
     public SelectQueryBuilder limit(int limit){
-        this.limitPart = limit;
+        this._limitPart = limit;
         return this;
     }
     @Override
     public String build(){
         StringBuilder sb = new StringBuilder();
 
-        if(!selectPart.isEmpty()){  // selectPart가 비워져 있지않다면
-            sb.append("SELECT ").append(String.join(", ", selectPart));
+        if(!_selectParts.isEmpty()){  // selectPart가 비워져 있지않다면
+            sb.append("SELECT ").append(String.join(", ", _selectParts));
         }
-        if(fromPart != null){  //fromPart가 비워져 있지 않다면
-            sb.append(" FROM ").append(fromPart);
+        if(_fromPart != null){  //fromPart가 비워져 있지 않다면
+            sb.append(" FROM ").append(_fromPart);
         }
-        if(wherePart != null){
-            sb.append(" WHERE ").append(wherePart);
+        if(!_whereParts.isEmpty()){
+            // note: 모든 조건을 " AND " 로 연결함
+            sb.append(" WHERE ").append(String.join(" AND ", _whereParts));
         }
-        if(orderByPart != null){
-            sb.append(" ORDER BY ").append(orderByPart);
+        if(_orderByPart != null){
+            sb.append(" ORDER BY ").append(_orderByPart);
         }
-        if(limitPart > 0){
-            sb.append(" LIMIT ").append(limitPart);
+        if(_limitPart > 0){
+            sb.append(" LIMIT ").append(_limitPart);
         }
 
         return sb.toString();
